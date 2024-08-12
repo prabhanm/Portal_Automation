a30769bf7fbd78bd402108f1e8b0e3898f2dadab
prabhanm <prmishra9313@gmail.com>
2024-08-12 12:51:14 +0900

    My 1st maven commit

---
+++ src/test/java/com/axa/test/MavenTest.java
@@ -0,0 +1,16 @@
+package com.axa.test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class MavenTest {
+	
+	@Test
+    public void shouldAnswerWithTrue()
+    {
+		System.out.println("This Test2 package");
+        assertTrue( true );
+    }
+
+}
